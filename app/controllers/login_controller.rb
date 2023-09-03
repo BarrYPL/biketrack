0575c71a31cf93bb0e52c4ac742b6cef26372cf6
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 <obartek4@gmail.com>
2023-09-03 15:18:36 +0200

    tests

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,9 +1,9 @@
 class LoginController < ApplicationController
     def redirect_oauth
-        redirect_to 'http://www.strava.com/oauth/authorize?client_id=113042&response_type=code&redirect_uri=http://www.biketrack.pro/authenticate&approval_prompt=auto&scope=read,profile:read_all,activity:read_all', allow_other_host: true
+        redirect_to 'http://www.strava.com/oauth/authorize?client_id=113042&response_type=code&redirect_uri=http://www.biketrack.pro/oauth-callback&approval_prompt=auto&scope=read,profile:read_all,activity:read_all', allow_other_host: true
     end
 
     def authenticate
-        puts params.inspect
+        @params = params
     end
 end
