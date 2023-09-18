fb2680f1197dc068d7702a203abb318fa33c25fa
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 <obartek4@gmail.com>
2023-09-19 00:58:40 +0200

    test

--- config/application.rb
+++ config/application.rb
@@ -10,11 +10,7 @@ module Biketrack
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
-    console do
-      config.conosole.whitelist = '*'
-    end
-
+    config.web_console.whitelisted_ips = '*'
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
