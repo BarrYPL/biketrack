5d1cd38bca6012ddeb96fedfb23ab7687e5ef223
bosuch <obartek4@gmail.com>
2023-09-19 09:18:13 +0200

    test

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,10 +1,10 @@
 class LoginController < ApplicationController
     def redirect_oauth
-        redirect_to 'http://www.strava.com/oauth/authorize?client_id=113042&response_type=code&redirect_uri=https://biketrack.pro/oauth-callback&approval_prompt=auto&scope=read,profile:read_all,activity:read_all', allow_other_host: true
+        redirect_to 'https://www.strava.com/oauth/authorize?client_id=113042&response_type=code&redirect_uri=https://biketrack.pro/oauth-callback&approval_prompt=auto&scope=read,profile:read_all,activity:read_all', allow_other_host: true
     end
 
     def authenticate
-        strava_key_handler = "www.strava.com/oauth/token"
+        strava_key_handler = "https://www.strava.com/oauth/token"
         puts "Params:"
         puts params[:code]
         response = RestClient.post(strava_key_handler, {:client_id => '113042', :client_secret => 'c9fb38720c6838d9f42ef6bace73ca694f948eaa', :code => params[:code], :grant_type => 'authorization_code'})        
