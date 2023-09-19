9dcbcaf618aa0b654b835307174ed1a293da8558
bosuch <obartek4@gmail.com>
2023-09-19 08:01:35 +0200

    test

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -5,8 +5,9 @@ class LoginController < ApplicationController
 
     def authenticate
         strava_key_handler = "https://www.strava.com/oauth/token"
+        puts "Params:"
+        puts params.inspect
         response = RestClient.post(strava_key_handler, {:client_id => '113042', :client_secret => 'c9fb38720c6838d9f42ef6bace73ca694f948eaa', :code => params[:code], :grant_type => 'authorization_code'})        
-        puts response.body
         @params = JSON.parse(response.body)
         if UserInfo.exists?(athlete_id: @params["athlete"]["id"])
             loggedin_user = UserInfo.find_by(athlete_id: @params["athlete"]["id"])
