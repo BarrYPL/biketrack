2e8687a48c9b2c77c96f8b2e3da6b4dd5fc1a2c2
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 <obartek4@gmail.com>
2023-09-21 00:19:40 +0200

    test

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -5,8 +5,7 @@ class LoginController < ApplicationController
 
     def authenticate
         strava_key_handler = "https://www.strava.com/oauth/token"
-        connection = Excon.new(strava_key_handler)
-        response = connection.post(:client_id => '113042', :client_secret => 'c9fb38720c6838d9f42ef6bace73ca694f948eaa', :code => params[:code], :grant_type => 'authorization_code')
+        response = Excon.post(strava_key_handler, :client_id => '113042', :client_secret => 'c9fb38720c6838d9f42ef6bace73ca694f948eaa', :code => params[:code], :grant_type => 'authorization_code')
         puts response.body
         @params = JSON.parse(response.body)
     end
