f03f6e370efa319b3782ad686991a81434692c5b
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 <obartek4@gmail.com>
2023-09-27 23:55:26 +0200

    Im missing C compiler

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -83,7 +83,7 @@ class LoginController < ApplicationController
                 @last_ride['max_speed'] = "--"
             end
             #get max values
-            if Ride.where((athlete_id: @params['athlete']['id']).all.count > 0)
+            if (Ride.where(athlete_id: @params['athlete']['id']).all.count > 0)
                 @max_speed = Ride.where(athlete_id: @params['athlete']['id']).order(max_speed: :desc).first
                 @longest_ride = Ride.where(athlete_id: @params['athlete']['id']).order(distance: :desc).first
                 @max_total_elevation_gain = Ride.where(athlete_id: @params['athlete']['id']).order(total_elevation_gain: :desc).first
