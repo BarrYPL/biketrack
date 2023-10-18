f0af8a3a598b0dc03bbf09944b898d56963cc3d9
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 <obartek4@gmail.com>
2023-10-18 23:54:13 +0200

    test

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -59,14 +59,9 @@ class LoginController < ApplicationController
             end
             @last_ride = Ride.where(athlete_id: @params['athlete']['id']).order(timestamp: :desc).first
             unless @last_ride.nil?
-                puts @last_ride['timestamp']
-                @last_ride['timestamp'] = Time.at(@last_ride['timestamp'])#.strftime("%A, %B %d, %Y")
-                puts Time.at(@last_ride['timestamp'])
-                puts @last_ride['timestamp']
+                @last_ride['timestamp'] = (Time.at(@last_ride['timestamp']).strftime("%A, %B %d, %Y"))
                 @last_ride['distance'] = (@last_ride['distance'].to_f / 1000.0).round(2)
-                @last_ride['moving_time'] = format_time(@last_ride['moving_time'])
-                puts format_time(@last_ride['moving_time'])
-                puts @last_ride['moving_time']
+                @last_ride['moving_time'] = (format_time(@last_ride['moving_time']))
                 @last_ride['total_elevation_gain'] = (@last_ride['total_elevation_gain']).round(0) 
                 @last_ride['average_speed'] = (@last_ride['average_speed'] * 3.6).round(2) 
                 @last_ride['max_speed'] = (@last_ride['max_speed'] * 3.6).round(2)
