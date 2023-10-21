23a22c5032c08e32d37acdd1c4bbac05c87e3aee
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 <obartek4@gmail.com>
2023-10-22 00:42:31 +0200

    test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,7 @@
 class HomeController < ApplicationController
+    def index
+        if UserInfo.find_by?(athlete_id: session[:current_user_id])
+            redirect_to user_profile_url
+        end
+    end
 end
