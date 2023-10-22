5cce36e00006beb2fec47e07dd28f1787f057f0f
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 <obartek4@gmail.com>
2023-10-22 19:37:34 +0200

    test

--- app/controllers/chains_controller.rb
+++ app/controllers/chains_controller.rb
@@ -22,7 +22,7 @@ class ChainsController < ApplicationController
 
   # POST /chains or /chains.json
   def create
-    puts "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx #{params['chain']['bike']}"
+    @bike = Bike.find_by(bike_id: params['chain']['bike'])
     @chain = @bike.chains.build(chain_params)
     respond_to do |format|
       if @chain.save
@@ -67,7 +67,7 @@ class ChainsController < ApplicationController
     def set_bike
       @bike = Bike.find_by(bike_id: params['bike'])
       if @bike.nil?
-        #redirect_to homepage_url, alert: "You probably doesn't have bikes added yet."
+        redirect_to homepage_url, alert: "You probably doesn't have bikes added yet."
       end
     end
 
