9923228d4213c3445d6708c78233de2d73b83da6
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 <obartek4@gmail.com>
2023-10-22 15:26:19 +0200

    Tests

--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -104,17 +104,7 @@ class UserProfileController < ApplicationController
     def add_bike_to_db(params_hash)
         unless Bike.exists?(bike_id: params_hash['id'])
             @user = UserInfo.find_by(athlete_id: session[:current_user_id])
-            @user.bikes.create!(
-                bike_id: params_hash['id'],
-                bike_name: params_hash['name'],
-                brand: params_hash['brand_name'],
-                user_info_id: session[:current_user_id],
-                resource_state: params_hash['resource_state'],
-                distance: params_hash['distance'],
-                bike_model_name: params_hash['model_name'],
-                frame_type: params_hash['frame_type'],
-                description: params_hash['description']
-            )
+            puts @user
         end
     end
 
