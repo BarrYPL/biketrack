0f1b70f45dbeab5a051d5befbe7ce1e78a7ffa6c
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 <obartek4@gmail.com>
2023-10-24 21:58:30 +0200

    testing config.yml

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,11 +1,11 @@
 class LoginController < ApplicationController
     def redirect_oauth
-        redirect_to 'https://www.strava.com/oauth/authorize?client_id=113042&response_type=code&redirect_uri=https://biketrack.pro/oauth-callback&approval_prompt=auto&scope=read,profile:read_all,activity:read_all', allow_other_host: true
+        redirect_to "https://www.strava.com/oauth/authorize?client_id=#{stored_client_id}&response_type=code&redirect_uri=https://biketrack.pro/oauth-callback&approval_prompt=auto&scope=read,profile:read_all,activity:read_all", allow_other_host: true
     end
 
     def authenticate
         strava_key_handler = "https://www.strava.com/oauth/token"
-        response = Excon.post(strava_key_handler, :body => URI.encode_www_form(:client_id => '113042', :client_secret => 'c9fb38720c6838d9f42ef6bace73ca694f948eaa', :code => params[:code], :grant_type => 'authorization_code'))
+        response = Excon.post(strava_key_handler, :body => URI.encode_www_form(:client_id => stored_client_id, :client_secret => stored_client_secret, :code => params[:code], :grant_type => 'authorization_code'))
         @params = JSON.parse(response.body)
         if (@params.has_key?("message"))
             redirect_to homepage_url
