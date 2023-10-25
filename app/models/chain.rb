52a2ee084d2e7149812f01d7c6498c99c0fde51c
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 <obartek4@gmail.com>
2023-10-26 00:10:18 +0200

    tests

--- app/models/chain.rb
+++ app/models/chain.rb
@@ -11,7 +11,7 @@ class Chain < ApplicationRecord
         if self.changed_timestamp_changed?
             puts "Chain #{self.id} has changed."
             self.bike.chains.where.not(id: self.id).update_all(is_actually_used: false)
-            last_changed_chain = self.bike.chains.order(:changed_timestamp).first
+            last_changed_chain = self.bike.chains.order(:instalation_date).first
             last_changed_chain.update(is_actually_used: true) if last_changed_chain
         end
     end
