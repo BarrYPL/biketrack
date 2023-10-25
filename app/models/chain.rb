c70f83b8ef50097e3a06abc2bec1dcac32fb0fbc
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 <obartek4@gmail.com>
2023-10-26 00:49:11 +0200

    test

--- app/models/chain.rb
+++ app/models/chain.rb
@@ -1,6 +1,7 @@
 class Chain < ApplicationRecord
     belongs_to :bike
     before_save :update_chain_status
+    after_save :uptade_active_chain
 
     scope :active_chain, -> { where(is_actually_used: true)}
 
@@ -9,9 +10,12 @@ class Chain < ApplicationRecord
     def update_chain_status
         if self.instalation_date_changed?
             self.bike.chains.where.not(id: self.id).update_all(is_actually_used: false)
-            last_changed_chain = self.bike.chains.order(:instalation_date).first
-            binding.pry
-            last_changed_chain.update(is_actually_used: true) if last_changed_chain
         end
+    end
+
+    def uptade_active_chain 
+        last_changed_chain = self.bike.chains.order(:instalation_date).first
+        binding.pry
+        last_changed_chain.update(is_actually_used: true) if last_changed_chain
     end
 end
