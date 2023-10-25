14e91d5736681a4976736f605a6119f2cc270ea6
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 <obartek4@gmail.com>
2023-10-25 23:39:37 +0200

    debuging

--- app/models/chain.rb
+++ app/models/chain.rb
@@ -1,5 +1,14 @@
 class Chain < ApplicationRecord
     belongs_to :bike
+    after_save :update_chain_status
 
     scope :active_chain, -> { where(is_actually_used: true)}
+
+    private
+  
+    def update_chain_status
+        self.bike.chains.update_all(is_actually_used: false)
+        last_changed_chain = self.bike.chains.order(:changed_timestamp).first
+        last_changed_chain.update(is_actually_used: true) if last_changed_chain
+    end
 end
