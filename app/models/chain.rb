142e600a3ccea77a6ca11f64eb33e8c5b9e6cbe8
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 <obartek4@gmail.com>
2023-10-26 00:41:36 +0200

    test

--- app/models/chain.rb
+++ app/models/chain.rb
@@ -8,9 +8,9 @@ class Chain < ApplicationRecord
   
     def update_chain_status
         if self.instalation_date_changed?
-            puts "Chain #{self.id} has changed."
             self.bike.chains.where.not(id: self.id).update_all(is_actually_used: false)
             last_changed_chain = self.bike.chains.order(:instalation_date).first
+            binding.pry
             last_changed_chain.update(is_actually_used: true) if last_changed_chain
         end
     end
