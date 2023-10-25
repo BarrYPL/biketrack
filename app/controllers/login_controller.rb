902d99b52a724585294f8671af8c2c479318fa90
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 <obartek4@gmail.com>
2023-10-25 22:54:44 +0200

    debuging

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -8,6 +8,7 @@ class LoginController < ApplicationController
         response = Excon.post(strava_key_handler, :body => URI.encode_www_form(:client_id => "113042", :client_secret => "c9fb38720c6838d9f42ef6bace73ca694f948eaa", :code => params[:code], :grant_type => 'authorization_code'))
         @params = JSON.parse(response.body)
         if (@params.has_key?("message"))
+            binding.pry
             redirect_to homepage_url
         else
             unless UserInfo.exists?(athlete_id: @params["athlete"]["id"])
