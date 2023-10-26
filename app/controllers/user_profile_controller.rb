9bcbf8e03aae4663b58bcf9eed1d9517e364e055
bosuch <obartek4@gmail.com>
2023-10-26 08:26:09 +0200

    tests

--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -120,7 +120,6 @@ class UserProfileController < ApplicationController
     end
 
     def add_ride_to_db(params_hash)
-        binding.pry
         params_hash.each do |ride_hash|
             unless Ride.exists?(ride_id: ride_hash['id'])
                 Ride.create!(
