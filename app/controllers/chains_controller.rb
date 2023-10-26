3bbe9383f9f3dc6e26347c88c8f1ff9a926c249f
bosuch <obartek4@gmail.com>
2023-10-26 13:39:43 +0200

    test

--- app/controllers/chains_controller.rb
+++ app/controllers/chains_controller.rb
@@ -84,7 +84,7 @@ class ChainsController < ApplicationController
     def km_since_last_vaxking(chain)
       unless chain.vaxed_timestamp.nil?
         #sum all km from vaxing date to now on specified bike
-        return Ride.where(gear_id: chain.bike.bike_id).where("start_date > ?", chain.vaxed_timestamp).sum(:distance)
+        return Ride.where(gear_id: chain.bike.bike_id).where("timestamp > ?", chain.vaxed_timestamp.to_i).sum(:distance)
       end
     end
 
