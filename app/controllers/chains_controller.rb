483d7bc0116221aff1f44970b0d1c46e49938200
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 <obartek4@gmail.com>
2023-10-29 20:31:50 +0100

    test

--- app/controllers/chains_controller.rb
+++ app/controllers/chains_controller.rb
@@ -1,6 +1,6 @@
 class ChainsController < ApplicationController
   before_action :set_chain, only: %i[ show edit update destroy ]
-  before_action :set_bike, only: [:index, :new, :show ]
+  before_action :set_bike, only: [:index, :new ]
   before_action :prepare_chart, only: [:index]
 
   # GET /chains or /chains.json
