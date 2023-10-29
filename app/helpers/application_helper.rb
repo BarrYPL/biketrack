d721d05430605f2d24b64ddd59c94fad072d8142
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 <obartek4@gmail.com>
2023-10-29 20:35:43 +0100

    test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
         unless chain.nil?
             unless chain.vaxed_timestamp.nil?
                 #sum all km from vaxing date to now on specified bike
-                return (Ride.where(gear_id: chain.bike.bike_id).where("timestamp > ?", chain.vaxed_timestamp.to_i).sum(:distance).to_f / 1000.0).round(2)
+                return (Ride.where(gear_id: chain.bike.bike_id).where("timestamp < ?", chain.vaxed_timestamp.to_i).sum(:distance).to_f / 1000.0).round(2)
             end
         end
     end
