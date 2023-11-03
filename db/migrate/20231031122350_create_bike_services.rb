9257b707d1e2247b7bbed7f0ad62a279610ca21b
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 <obartek4@gmail.com>
2023-11-03 19:36:41 +0100

    test

--- db/migrate/20231031122350_create_bike_services.rb
+++ db/migrate/20231031122350_create_bike_services.rb
@@ -1,9 +1,10 @@
 class CreateBikeServices < ActiveRecord::Migration[7.0]
   def change
     create_table :bike_services do |t|
-      t.service_name :string
-      t.service_description :string
-      t.service_date :date
+      t.string :service_name
+      t.string :service_description
+      t.date :service_date
+      t.integer :bike_id
       t.timestamps
     end
   end
